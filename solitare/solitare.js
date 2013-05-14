755befdc3e1abac5ed9c4fe6e755ba9e4cde6405
Goyatuzo <yotagur@gmail.com>
2013-05-14 16:23:02 -0500

    Added the init logic in solitare.js.

--- solitare/solitare.js
+++ solitare/solitare.js
@@ -1,4 +1,47 @@
-function main()
+// The card class.
+function Card( number, symbol )
 {
-	// Code for the game loop goes here.
+	this.number = number;
+	this.symbol = symbol;
+}
+
+var theDeck;
+
+function init()
+{
+	theDeck = new Array( 52 );
+
+	// Create the spades.
+	for( var i = 1; i < 14; i++ )
+		theDeck[ i - 1 ] = new Card( i, "Spade" );
+
+	// Create the hearts.
+	for( var i = 1; i < 14; i++ )
+		theDeck[ 13 + i ] = new Card( i, "Heart" );
+
+	// Create the clubs.
+	for( var i = 1; i < 14; i++ )
+		theDeck[ 26 + i ] = new Card( i , "Club" );
+
+	// Create the hearts.
+	for( var i = 1; i < 14; i++ )
+		theDeck[ 39 + i ] = new Card( i, "Diamond" );
+
+	// Randomize the deck.
+	for( var i = 0; i < 52; i++ )
+		swap( theDeck[ Math.floor( Math.random() * 53 ) ],
+				theDeck[ Math.floor( Math.random() * 53 ) ] );
+}
+
+function swap( a, b )
+{
+	var temp = a;
+
+	a = b;
+	b = temp;
+}
+
+function drag()
+{
+
 }
