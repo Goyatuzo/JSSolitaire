7475ac2503278d83aff868c624f10492a189b92d
Adam <a.smith@isharemedical.com>
2013-05-14 21:50:02 -0500

    started implementing stacking

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ $(document).ready(function(){
     	containment: '#gameboard'
     	}
     );
+    
+    /*$(".sample-card-design").droppable({
+		drop: function (event, ui) {
+			//functionality when dropping
+			
+		}, 
+		over: function( event, ui ) { 
+			$("#info").html("moving in!"); 
+		}, 
+		out: function( event, ui ) { 
+			$("#info").html("moving out!"); 
+		} 
+	});*/
+	
+	$('.sample-card-design').droppable({
+    	drop: function(ev, ui) {
+       		$(ui.draggable).detach().css({top: 20,left: 0}).appendTo(this);
+    	},
+    	out: function(ev, ui) {
+	    	$(ui.draggable).detach(this);
+		}
+	});
+    
 });
