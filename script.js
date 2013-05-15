210f74563b9bff3f73b57a35892287bf2291cea5
Goyatuzo <yotagur@gmail.com>
2013-05-15 15:48:21 -0500

    Added a comparison function to check to check for red/black alternation.

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ $(document).ready(function(){
 		accept: '.sample-card-design',
 		drop: cardHandler
 	});
+
+	function isOpposingColors( cardOne, cardTwo )
+	{
+		var oneRank = cardOne.rank;
+		var twoRank = cardTwo.rank;
+
+		// If the first rank is "black", return whether or not the other is "red"
+		if( oneRank == "spade" || oneRank == "club" )
+			return ( twoRank == "heart" || twoRank == "diamond" );
+		// If the first rank is "red", return whether or not the other is "black"
+		else if( oneRank == "heart" || oneRank == "diamon" )
+			return ( twoRank == "spade" || twoRank == "club" );
+		// If anything else is happening, oh no.  Problem.
+		else
+			return false;
+	}
 	
 	// This function handles what cards can be stacked ontop of each other.
 	function cardHandler(ev, ui) {
-       		$(ui.draggable).css({top: 30,left: 10}).appendTo(this);
+			if(  )
+       			$(ui.draggable).css({top: 30,left: 10}).appendTo(this);
    	}
     
 });
