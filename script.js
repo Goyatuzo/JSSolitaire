3fe4d78a0ac5d8e1aca1a6c370e260aebf5a8fe4
Adam <a.smith@isharemedical.com>
2013-05-15 01:04:58 -0500

    added initialization of deck

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
 $(document).ready(function(){
-    //Make the <div> with id 'aceofhearts' draggable here 
-    $('.sample-card-design').draggable({
-    	containment: '#gameboard'
-    	}
-    );
-    
-    /*$(".sample-card-design").droppable({
-		drop: function (event, ui) {
-			//functionality when dropping
-			
-		}, 
-		over: function( event, ui ) { 
-			$("#info").html("moving in!"); 
-		}, 
-		out: function( event, ui ) { 
-			$("#info").html("moving out!"); 
-		} 
-	});*/
 	
 	$('.stackContainer').droppable({
 		accept: '.sample-card-design',
-		drop: function(ev, ui) {
-       		$(ui.draggable).detach().appendTo(this);
-   		}
+		drop: cardHandler
 	});
 	
-	$('.sample-card-design').droppable({
-		accept: '.sample-card-design',
-    	drop: function(ev, ui) {
-       		$(ui.draggable).detach().css({top: 30,left: 0}).appendTo(this);
-    	},
-    	greedy: true,
-    	out: function(ev, ui) {
-	    	//$(ui.draggable).clone().appendTo('#gameboard');
-	    	//$(ui.draggable).clone().appendTo('#gameboard').draggable();
-		}
-	});
+	function cardHandler(ev, ui) {
+       		$(ui.draggable).css({top: 30,left: 10}).appendTo(this);
+   	}
     
 });
--- solitare/solitare.js
+++ solitare/solitare.js
@@ -1,41 +1,43 @@
 // The card class.
-function Card( number, symbol )
+function Card( rank, suit )
 {
-	this.number = number;
-	this.symbol = symbol;
+	this.rank = rank;
+	this.suit = suit;
 }
 
 var theDeck;
+
+ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
 function init()
 {
 	theDeck = new Array( 52 );
 
 	// Create the spades.
-	for( var i = 1; i < 14; i++ )
-		theDeck[ i - 1 ] = new Card( i, "Spade" );
+	for( var i = 0; i < ranks.length; i++ )
+		theDeck[ i ] = new Card( ranks[i], "spade" );
 
 	// Create the hearts.
-	for( var i = 1; i < 14; i++ )
-		theDeck[ 12 + i ] = new Card( i, "Heart" );
+	for( var i = 0; i < ranks.length; i++ )
+		theDeck[ 13 + i ] = new Card( ranks[i], "heart" );
 
 	// Create the clubs.
-	for( var i = 1; i < 14; i++ )
-		theDeck[ 25 + i ] = new Card( i , "Club" );
+	for( var i = 0; i < ranks.length; i++ )
+		theDeck[ 26 + i ] = new Card( ranks[i], "club" );
 
 	// Create the hearts.
-	for( var i = 1; i < 14; i++ )
-		theDeck[ 38 + i ] = new Card( i, "Diamond" );
+	for( var i = 0; i < ranks.length; i++ )
+		theDeck[ 39 + i ] = new Card( ranks[i], "diamond" );
 
 	/**
-	 * Randomize the deck by essentially swapping the deck N number
+	 * Randomize the deck by essentially swapping the deck N rank
 	 * of times.  Figured it was more efficient.
 	 */
 	for( var i = 0; i < 52; i++ )
 	{
 		// Because javascript has no pass by reference.
-		var idxOne = Math.floor( Math.random() * 53 );
-		var idxTwo = Math.floor( Math.random() * 53 );
+		var idxOne = Math.floor( Math.random() * 52 );
+		var idxTwo = Math.floor( Math.random() * 52 );
 
 		var temp = theDeck[ idxOne ];
 
@@ -43,3 +45,30 @@ function init()
 		theDeck[ idxTwo ] = temp;		
 	}
 }
+
+function makeCard(Card) {
+	
+	
+	return $('<div class="sample-card-design" id="kd"> <div class="cardbackground"> <div class="' + Card.suit + '"style="top:8.3333%;left:17.5%"></div> <div class="cardIdentifier">' + Card.rank + '</div> <div class="' + Card.suit + '" style="bottom:3.3333%;right:2%"></div> </div> </div>' )
+}
+ 
+
+$(document).ready(function() {
+	init();
+  for ( var i=0; i<theDeck.length; i++ ) {
+	  makeCard(theDeck[i]).appendTo('#gameboard').draggable( {
+      containment: '#gameboard',
+  	}).droppable({
+		accept: '.sample-card-design',
+    	drop: function(ev, ui) {
+       		$(ui.draggable).detach().css({top: 30,left: 0}).appendTo(this);
+    	},
+    	greedy: true,
+    	out: function(ev, ui) {
+	    	//$(ui.draggable).clone().appendTo('#gameboard');
+	    	//$(ui.draggable).clone().appendTo('#gameboard').draggable();
+		}
+	});
+  }
+});
+
